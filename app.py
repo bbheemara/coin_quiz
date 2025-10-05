96f0e956bc33cf226dd6df60c18a396a105ddd30
bbheemara <bbheemaraya944@gmail.com>
2025-10-05 14:20:49 +0530

    Added Home page with a button to start quiz, added few questions, tried changing fonts which couldn't work out:(, added new routes

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for, session,redirect
+from questions import questions
 
 app = Flask(__name__)
-
+app.secret_key = "hmmwhatisthiss..."
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/start')
+def start():
+    session["score"] = 0
+    session["streak"] = 0
+    session["q_index"] = 0
+    return redirect(url_for("quiz"))
+
+@app.route('/quiz',methods=["GET", "POST"])
+def quiz():
+    ques_index = session.get("q_index", 0)
+
+
 
 if __name__ == '__main__':
     
---
+++ questions.py
@@ -0,0 +1,5 @@
+questions = [
+    {"text": "Who is the main host of Siege?", "A": "Olive", "B": "Zack", "correct": "A"},
+    {"text": "What is coins/shells are called in Siege?", "A": "Coffie", "B": "coffers", "correct": "B"},
+    {"text": "What is the Minimum req hours for Siege per week?", "A": "18", "B": "10", "correct": "B"},
+]
